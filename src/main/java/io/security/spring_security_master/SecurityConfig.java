4e33978a7528521ad32359832b9b3d180f088333
MinSang22Kim <powerminsang9901@gmail.com>
2024-06-29 15:02:53 +0900

    섹션2. 인증 프로세스 - anonymous

--- src/main/java/io/security/spring_security_master/SecurityConfig.java
+++ src/main/java/io/security/spring_security_master/SecurityConfig.java
@@ -25,23 +25,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginPage("/loginPage")
-                        .loginProcessingUrl("/loginProc")
-                        .defaultSuccessUrl("/", true)
-                        .failureUrl("/failed")
-                        .usernameParameter("userId")
-                        .passwordParameter("passwd")
-                        .successHandler((request, response, authentication) -> {
-                            System.out.println("authentication : " + authentication);
-                            response.sendRedirect("/home");
-                        })
-                        .failureHandler((request, response, exception) -> {
-                            System.out.println("exception : " + exception.getMessage());
-                            response.sendRedirect("/login");
-                        })
-                        .permitAll()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/anonymous").hasRole("GUEST")
+                        .requestMatchers("/anonymousContext", "/authentication").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .anonymous(anonymous -> anonymous
+                        .principal("guest")
+                        .authorities("ROLE_GUEST")
                 );
         return http.build();
     }
